5592feb3dc0bd423dd4d41d6d44bb50ce32e3f03
Linda Barbiero <linda.barbiero.st@everis.com>
2019-10-09 09:56:43 +0100

    added a new person class and then tested

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 console.log("Hello World from NodeJS");
 console.log("Trying something new");
+
+const person = require("./person.js");
+console.log("Found a new person : "+person.name+" - "+person.age+" years old.");
---
+++ person.js
@@ -0,0 +1,6 @@
+const person = {
+    name : "John",
+    age : 30
+}//Person
+
+module.exports = person;
