6c5193876cc15947b551e3e8e071e71f989c68b7
Linda Barbiero <linda.barbiero.st@everis.com>
2019-10-09 11:25:23 +0100

    Read and modify a file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -12,3 +12,21 @@ fs.writeFile(path.join(__dirname,'/test','hello.txt'),'Hello world!\nI\'m Linda,
     if (err) throw err;
     console.log('File written');
 });
+
+//Add new content to a File
+fs.appendFile(path.join(__dirname,'/test','hello.txt'),'\nAdded a new part to the file!', err => {
+    if (err) throw err;
+    console.log('File re-written');
+});
+
+//Read a File
+fs.readFile(path.join(__dirname,'/test','hello.txt'),'utf8',function(err, data){
+    if(err) throw err;
+    console.log(data);
+});
+
+//Rename a File
+fs.rename(path.join(__dirname,'/test','hello.txt'),path.join(__dirname,'/test','helloworld.txt'), err => {
+    if (err) throw err;
+    console.log('File renamed');
+});
